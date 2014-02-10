0b37472011456519e498fca86c900221d4315a3c
Krista Perks <kperky@gmail.com>
2014-02-10 14:09:24 -0800

    changed the binsize for FanoAnalysis

--- m_files/rasterionto.m
+++ m_files/rasterionto.m
@@ -1,11 +1,13 @@
-
+% 10feb2014 - KP
+%     -changed stimpath to work on local computer Krista's Macbook Pro
+%     -changed binsize from 50msec to 1msec to work with FanoFactorAnalysis
 
 
 function [ax]=rasterionto(toelist, start,stop, blocksize,blocktypes,sitetype, smooth, doprintout,dosave)
 stimtimes = [ 9.915 9.082 10.432 9.225 9.273 9.579 9.334 9.292 9.654 9.078 10.257 10.532 10.135 10.611 10.719 10.124 10.532 10.719 10.124 10.160 9.954 10.155 9.000 10.940 10.571 9.932 1.000 1.000 1.000 1.000 1.000 1.000 5.0000 5.0000 5.0000 5.0000 5.0000 5.0000 5.0000 5.0000];
 stimnames = {'A1'; 'A2'; 'A3'; 'A4'; 'A5'; 'B1'; 'B2'; 'B3'; 'B4'; 'B5'; 'N1'; 'N2'; 'N3'; 'N4'; 'N5'; 'N6'; 'C1'; 'C2'; 'D1'; 'D2'; 'E1'; 'E2'; 'S1'; 'S2'; 'H1'; 'H2'; '2'; '4'; '6'; '8'; '10'; '12'; 'A2short'; 'A4short'; 'B1short'; 'B2short'; 'D1short'; 'D2short'; 'E1short'; 'E2short'};
 figure
-binsize = 50; %in msec
+binsize = 1; %in msec
 bps = 1000/binsize;
 %stimpath = '~/stims/gng_motifs';
 stimpath = '/Users/kperks/GitHub/iontoncm/forstrf/strfstims';
